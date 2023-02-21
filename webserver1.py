016c9fc04004a23925372160f35b216f11476c8b
dshirbh1 <dshirbh1@binghamton.edu>
2023-02-21 13:59:05 -0500

    Added basic connection setup and sending a msg/404 on client

---
+++ webserver1.py
@@ -0,0 +1,56 @@
+# import socket module
+from socket import *
+
+# In order to terminate the program
+import sys
+
+# Prepare a sever socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+### YOUR CODE HERE ###
+serverPort = 59354
+# if serverport is in user; unbind and 
+# then bind
+# ; if not bind
+serverAddress = "149.125.123.98"
+serverSocket.bind((serverAddress,serverPort))
+serverSocket.listen(1)
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+
+        #Adding a break event to come out of while loop
+        
+        f = open(filename[1:])
+        outputdata = f.read()
+
+        # Send one HTTP header line into socket
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+
+        # Send the content of the requested file into socket
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        # Close client socket
+        connectionSocket.close()
+
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("File not found".encode())
+
+        # Close client socket
+        connectionSocket.close()
+
+# Close server socket
+serverSocket.close()
+
+# Terminate the program after sending the corresponding data
+sys.exit()
